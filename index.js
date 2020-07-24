0f58f371c0a286960dd6dcec473297827cf408b4
JP Carey <jpcarey4@gmail.com>
2020-07-24 10:34:22 -0600

    target and use remove method on eightball image on click

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var input = document.querySelector('.input-form');
 var inputButton = document.querySelector('.input-button');
 var questionRepeat = document.querySelector('h3');
 var questionAnswer = document.querySelector('h2');
+var eightBall = document.querySelector('.eight-ball')
 
 var answers = [
   "It is certain.",
@@ -37,4 +38,5 @@ function giveAnswer() {
   questionRepeat.innerText = input.value;
   questionAnswer.innerText = answers[getRandomIndex(answers)];
   input.value = "";
+  eightBall.remove();
 };
